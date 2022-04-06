66540775623babac1c14d785f92794bf0ce662f0
Joker <victorassagra@hotmail.com>
2022-04-05 22:24:27 -0300

    Projetos da Alura com o curso básico de java

---
+++ While_for/src/main/java/com/mycompany/while_for/Main.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.while_for;
+
+/**
+ *
+ * @author victo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        /*
+        Dessa vez, Clarice resolveu desafiar Fernando a transformar o código a seguir que utiliza um laço while em um código que utilize o for. 
+        Você consegue ajudar Fernando a chegar a resposta do desafio?
+        
+        
+        int contador = 0;
+        while (contador <= 10) {
+            System.out.println(contador);
+            contador++;
+        }
+        
+        
+*/
+        
+        
+        for(int contadorFor=0;contadorFor<=10;contadorFor++){
+            System.out.println(contadorFor);
+        }
+    }
+
+}
